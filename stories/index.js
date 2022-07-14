8ad2eaba7bf8e8fac78d7ff83d099a4d82011c52
Kenny Moon <kennymoon90@gmail.com>
2022-07-13 19:25:10 -0700

    Added stories to test each component of the Appointment individually

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 
 storiesOf("Button", module)
@@ -146,4 +150,14 @@ storiesOf("Button", module)
         .add("Header", () => <Header time="12pm" />)
         .add("Empty", () => (
           <Empty onAdd={action("onAdd")}/>
+        ))
+        .add("Show", () => (
+          <Show onEdit={action("onEdit")} onDelete={action("onDelete")} student="Lydia Miller-Jones" interviewer={interviewer}/>
+        ))
+        .add("Confrim", () => (
+          <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")} message="Delete the appointment?"/>
+        ))
+        .add("Status", () => <Status message="Deleting"/>)
+        .add("Error", () => (
+          <Error onClose={action("onClose")} message="Could not delete appointment"/>
         ))
