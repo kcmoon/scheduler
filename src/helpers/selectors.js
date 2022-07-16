69ff16b90e79df2fc8524d97202b2575af19749f
Kenny Moon <kennymoon90@gmail.com>
2022-07-15 20:22:36 -0700

    Created getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -33,3 +33,23 @@ export function getInterview(state, interview) {
 
   return interviewObj;
 };
+
+export function getInterviewersForDay(state, day) {
+  // created empty array to push filtered interviewers
+  let filteredInterviewers = [];
+  // filtered the days array of the state object to match the day provided and assigned object to variable filteredDayObj
+  const filteredDayObj = state.days.filter(d => d.name === day)[0];
+
+  // if filteredDayObj is undefined or the days array in the object is empty, return an empty array
+  if (filteredDayObj === undefined || state.days.length === 0) {
+    return [];
+  }
+  // loop through the interviewers array of the filteredDayObj by appointment id 
+  filteredDayObj.interviewers.forEach(interviewerId => {
+
+    // push all apppointments with the appointment id to the filteredinterviewers array
+    filteredInterviewers.push(state.interviewers[interviewerId])
+  });
+
+  return filteredInterviewers;
+};
